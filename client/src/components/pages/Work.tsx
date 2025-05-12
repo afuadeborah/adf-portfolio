0d176b8369ba3d791de875018d829cb8cdaa5076
Afua <afrimpong.15@gmail.com>
2025-05-11 23:08:27 -0400

    feat: continuing Work styling + layout

--- client/src/components/pages/Work.tsx
+++ client/src/components/pages/Work.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { WorkTabContent, WorkTabs } from "../../types";
 import { fetchWorkTabContent } from "../../api";
+import workIcon from "../../assets/images/icons/briefcase.png";
 
 const Work = () => {
 	// Handle tab selection
@@ -43,80 +44,98 @@ const Work = () => {
 	);
 
 	return (
-		<div className='work__window window-body'>
-			<menu role='tablist'>
-				{tabContent != null &&
-					tabContent.tabs.map((tab) => (
-						<li
-							key={tab.id}
-							role='tab'
-							aria-selected={activeTab === tab.id}
-						>
-							<button
-								onClick={() => handleTabClick(tab.id)}
-								value={`#${tab.id}`}
+		<div className='window work__window'>
+			<div className='title-bar'>
+				<h1 className='title-bar-text'>
+					<img src={workIcon} alt='Work icon' />
+					Work Experience & Skills
+				</h1>
+				<div className='title-bar-controls'>
+					<button aria-label='Close'></button>
+				</div>
+			</div>
+
+			<div className='window-body'>
+				<menu role='tablist'>
+					{tabContent != null &&
+						tabContent.tabs.map((tab) => (
+							<li
+								key={tab.id}
+								role='tab'
+								aria-selected={activeTab === tab.id}
 							>
-								{tab.company}
-							</button>
-						</li>
-					))}
-			</menu>
+								<button
+									onClick={() => handleTabClick(tab.id)}
+									value={`#${tab.id}`}
+								>
+									{tab.company}
+								</button>
+							</li>
+						))}
+				</menu>
 
-			<div className='window' role='tabpanel'>
-				<div className='window-body'>
-					{activeTabContent && (
-						<div className='work__content-container'>
-							<div className='d-flex'>
-								<div className='work__image-container mr-4'>
-									<img
-										src={`${serverUrl}${activeTabContent.imgUrl}`}
-										width={200}
-										height={200}
-										alt={`${activeTabContent.company} logo`}
-									/>
+				<div className='window' role='tabpanel'>
+					<div className='window-body'>
+						{activeTabContent && (
+							<div className='work__content-container'>
+								<div className='d-flex'>
+									<div className='work__image-container mr-4'>
+										<img
+											src={`${serverUrl}${activeTabContent.imgUrl}`}
+											width={200}
+											height={200}
+											alt={`${activeTabContent.company} logo`}
+										/>
+									</div>
+
+									<div className='work__experience-container p-2'>
+										<h1 className='work__company-title'>
+											{activeTabContent.company}
+										</h1>
+										<h2 className='my-3'>
+											{activeTabContent.title}
+										</h2>
+										<p className='work__location'>
+											{activeTabContent.location}
+										</p>
+										<span className='text-uppercase'>
+											{activeTabContent.date}
+										</span>
+									</div>
 								</div>
 
-								<div className='work__experience-container p-2'>
-									<h1 className='work__company-title'>
-										{activeTabContent.company}
-									</h1>
-									<h2 className='my-3'>
-										{activeTabContent.title}
-									</h2>
-									<p className='work__location'>
-										{activeTabContent.location}
-									</p>
-									<span className='text-uppercase'>
-										{activeTabContent.date}
-									</span>
-								</div>
+								<ul className='work__experience-list tree-view mt-3'>
+									<details open className='mt-2'>
+										<summary>What did I get up to?</summary>
+										<ul>
+											{activeTabContent.points.map(
+												(point, index) => (
+													<li key={`point-${index}`}>
+														{point}
+													</li>
+												)
+											)}
+										</ul>
+									</details>
+
+									<details className='mt-4'>
+										<summary>Top Skills</summary>
+										<ul>
+											{activeTabContent.skills.map(
+												(skill, index) => (
+													<li
+														key={`point-${index}-${index}`}
+													>
+														{skill}
+													</li>
+												)
+											)}
+										</ul>
+									</details>
+								</ul>
 							</div>
-
-							<ul className='work__experience-list tree-view mt-3'>
-								<details open className='mt-2'>
-									<summary>What did I get up to?</summary>
-									<ul>
-										{activeTabContent.points.map(
-											(point) => (
-												<li>{point}</li>
-											)
-										)}
-									</ul>
-								</details>
-
-								<details open className='mt-4'>
-									<summary>Top Skills</summary>
-									<ul>
-										{activeTabContent.skills.map(
-											(point) => (
-												<li>{point}</li>
-											)
-										)}
-									</ul>
-								</details>
-							</ul>
-						</div>
-					)}
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
