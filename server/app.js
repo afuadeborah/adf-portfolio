8de03ba5a4c7757de8fc31974e5d6cfc2dbf309a
Afua <afrimpong.15@gmail.com>
2025-05-14 20:26:23 -0400

    feat: building and styling the projects page refactor: refactoring how data is pulled into the server setup file chore: updating image files in the server chore: updating spacing

--- server/app.js
+++ server/app.js
@@ -1,114 +1,31 @@
 import express from "express";
 import cors from "cors";
+import { menuItems } from './data/menuItems.js';
+import { workTabContent } from './data/workTabContent.js';
+import { projectInfo } from './data/projectInfo.js';
 
 const app = express();
 const PORT = 5000;
 app.use(cors());
-
-// Menu items
-const menuItems = [
-    {
-        name: "About",
-        imgUrl: "/icons/book-about.png",        
-        url: "/about",
-        position: "LEFT"
-    },
-    {
-        name: "Work",
-        imgUrl: "/icons/briefcase.png",
-        url: "/work",
-        position: "LEFT"
-    },
-    {
-        name: "Projects",
-        imgUrl: "/icons/projects.png",
-        url: "/projects",
-        position: "LEFT"
-    },
-    {
-        name: "Contact",
-        imgUrl: "/icons/phone-contact.png",
-        url: "/contact",
-        position: "LEFT"
-    },
-    {
-        name: "Home",
-        imgUrl: "/icons/home.png",
-        url: "/",
-        position: "RIGHT"
-    },
-    {
-        name: "French",
-        imgUrl: "/icons/globe.png",
-        url: "",
-        position: "RIGHT"
-    },
-];
-
-// Work Page Tab Content
-const workTabContent = {
-    tabs: [
-        {
-            id: "work-1",
-            title: "Intermediate Frontend Developer",
-            company: "Asics Apps",
-            location: "Remote",
-            imgUrl: "/images/aa-logo.png",
-            date: "Aug 2021 - Oct 2024",
-            points: [
-                "Created pixel-perfect, reusable UI components based on Figma designs for the company's internal design system, improving consistency and collaboration across teams and company applications.",
-                "Rewrote legacy pages via component-based architecture React SPA (TypeScript and Remix.js) powering Race Roster's CRM feature resulting in improved performance and maintainability.",
-                "Developed and updated features for the core registration platform, mainly dynamic data-driven forms involving time, currency, and date handling in line with evolving business priorities, improving performance and reliability."
-            ],
-            skills: [
-                "React",
-                "TypeScript",
-                "PHP",
-                "GraphQL",
-                "Testing",
-                "Agile Methodology",
-                "CRM Management",
-                "Component Libraries",
-            ]
-        },
-                {
-            id: "work-2",
-            title: "Fullstack Developer",
-            company: "Art & Science",
-            location: "Toronto, Ontario + Remote",
-            imgUrl: "/images/as-logo.png",
-            date: "Aug 2020 - Aug 2021",
-            points: [
-                "Developed and maintained custom WordPress websites, including building themes, plugins, and responsive front-end interfaces tailored to client needs.",
-                "Enhanced legacy PHP applications by resolving bugs, improving performance, and introducing new features to ensure long-term maintainability.",
-                "Led demos and client-facing presentations, strengthening relationships and aligning development goals with stakeholder expectations.",
-                "Mentored and onboarded an intern to own future deployments and ongoing support."
-            ],
-            skills: [
-                "WordPress Development",
-                "PHP",
-                "Vue",
-                "PSD Design to Code",
-                "Mentorship",
-                "Accessibility Audits",
-            ]
-        }
-    ]
-};
 
 // serve the image files
 app.use(express.static('public'));
 
 // GET menu items
 app.get('/api/menu-items', (req, res) => {
-    res.json(menuItems);
+  res.json(menuItems);
 });
 
 // GET work page tab content
 app.get('/api/tab-content', (req, res) => {
-    res.json(workTabContent);
+  res.json(workTabContent);
+});
+
+// GET project information
+app.get('/api/projects', (req, res) => {
+  res.json(projectInfo);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 });
---
+++ server/data/projectInfo.js
@@ -0,0 +1,121 @@
+export const projectInfo = [
+    {
+        id: "windows",
+        repoUrl: "https://github.com/afuadeborah/windows-98-slack-clone",
+        imgUrl: "/projects/windows-98.png",
+        title: "Windows 98 Slack Clone",
+        tagline: "If you recognize this design, please start stretching daily.",
+        description: "A fullstack single page app that allows users to add, update, and delete posts. Initial posts come from the Slack Clone library, styling inspired by the 98.css library by Jordan Scales.",
+        stackTitles: [
+            "REST APIs",
+            "Python",
+            "React"
+        ],
+        status: "CURRENT",
+    },
+    {
+        id: "teardrop",
+        liveSiteUrl: "https://afuadeborah.github.io/teardrop/",
+        repoUrl: "https://github.com/afuadeborah/teardrop",
+        imgUrl: "/projects/teardrop.png",
+        title: "Tear(Drop)",
+        tagline: "Don't forget to look up.",
+        description: "A multiscreen experience in which users are encouraged to interact with the Control form and reflect on impactful experiences in their lives and their visualization in the Display. The display (using Particle.js) uses real-time updates from Firebase to update user input.",
+        stackTitles: [
+            "React",
+            "Firebase",
+            "SASS"
+        ],
+        status: "CURRENT",
+    },    
+    {
+        id: "afcodes",
+        liveSiteUrl: "https://afuadeborah.github.io/portfolio",
+        repoUrl: "https://github.com/afuadeborah/portfolio",
+        imgUrl: "/projects/portfolio.png",
+        title: "Afua Deborah Codes",
+        tagline: "Frontend Web Developer",
+        description: "My very own V1 portfolio from 2020. Made to showcase my projects from my early web dev days. Designed and fully coded my moi.",
+        stackTitles: [
+            "jQuery",
+            "SASS",
+            "Accessibility + Animation Libraries"
+        ],
+        status: "PAST",
+    },
+    {
+        id: "eyedeal",
+        liveSiteUrl: "https://afuadeborah.github.io/afua-frimpong-beyond-bootcamp/",
+        repoUrl: "https://github.com/afuadeborah/afua-frimpong-beyond-bootcamp",
+        imgUrl: "/projects/eyedeal.png",
+        title: "Eyedeal",
+        tagline: "Is your eyeliner on point?",
+        description: "An online store made with Vanilla JS (no external libraries!). Shop, add to your cart, and see your total. Products come from Makeup API.",
+        stackTitles: [
+            "SASS",
+            "Vanilla JavaScript",
+            "REST APIs",
+        ],
+        status: "PAST",
+    },
+    {
+        id: "gotmusic",
+        liveSiteUrl: "https://afuadeborah.github.io/got-music",
+        repoUrl: "https://github.com/afuadeborah/got-music",
+        imgUrl: "/projects/got-music.png",
+        title: "Got Music",
+        tagline: "Turn it on and turn it up.",
+        description: "An online store made with Vanilla JS (no external libraries!). Shop, add to your cart, and see your total. Products come from Makeup API.",
+        stackTitles: [
+            "SASS",
+            "Vanilla JavaScript",
+            "REST APIs",
+        ],
+        status: "PAST",
+    },
+    {
+        id: "todo",
+        repoUrl: "https://github.com/Julia-Dufresne-Afua-Frimpong/julia-dufresne-afua-frimpong",
+        imgUrl: "/projects/to-do-in-to.png",
+        title: "To Do In TO",
+        tagline: "What's the plan?",
+        description: "An event app that grabs entertainment events in Toronto from the Ticketmaster API for a user-selected date. Paired programming with Julia Dufresne.",
+        stackTitles: [
+            "jQuery",
+            "SASS",
+            "REST API"
+        ],
+        status: "PAST",
+    },
+    {
+        id: "harper",
+        liveSiteUrl: "https://afuadeborah.github.io/harper/index.html",
+        repoUrl: "https://github.com/afuadeborah/harper",
+        imgUrl: "/projects/harper.png",
+        title: "Everyday Life With Harper",
+        tagline: "Cotton candy vibes.",
+        description: "Multi-page PSD conversion made using a client brief and design files. Responsive across devices.",
+        stackTitles: [
+            "HTML5",
+            "SASS",
+            "Responsive Design"
+        ],
+        status: "PAST",
+    },
+    {
+        id: "tasty",
+        repoUrl: "https://github.com/afuadeborah/tasty-psd",
+        imgUrl: "/projects/tasty.png",
+        title: "Tasty",
+        tagline: "Floating isn't always fun, y'all.",
+        description: "PSD conversion made from client brief and design files using floats for layout. Responsive across devices.",
+        stackTitles: [
+            "HTML5",
+            "Floats",
+            "Responsive Design",
+        ],
+        status: "PAST",
+    },
+];
+
+
